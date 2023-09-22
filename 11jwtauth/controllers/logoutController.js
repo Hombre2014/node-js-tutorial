58bddc512b24ff6bea6f1290da3e633c4e2fadbb
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-09-22 15:27:59 +0200

    Add logout function

---
+++ 11jwtauth/controllers/logoutController.js
@@ -0,0 +1,35 @@
+const usersDB = {
+  users: require('../model/users.json'),
+  setUsers: function (data) { this.users = data }
+}
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+  // On client side delete also the access token
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); // No Content
+  const refreshToken = cookies.jwt;
+
+  // if RefreshToken in DB?
+  const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+  if (!foundUser) {
+    res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    return res.sendStatus(204);
+  }
+
+  // Delete the refreshToken from DB
+  const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+  const currentUser = { ...foundUser, refreshToken: '' };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, '..', 'model', 'users.json'),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // secure: true, serves only https. SameSite: 'None' serves cross-site requests
+  res.sendStatus(204);
+}
+
+module.exports = { handleLogout }
